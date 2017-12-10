a74aae34c29a8685507f96df073411a63330cb45
Michael Cook <entozoon@gmail.com>
2017-12-10 23:17:01 +0000

    Looking into teXtureCache although..
    It might not be the thing anymore - see:
    http://www.html5gamedevs.com/topic/16019-preload-all-textures/
    Despite it giving an explicit example:
    http://pixijs.io/examples/#/basics/spritesheet.js

--- src/Item.js
+++ src/Item.js
@@ -1,8 +1,28 @@
 import Pixi from "./PixiCreate";
 
+// MAYBE DON'T USE THIS ACTUALLY
+Pixi.engine.loader.add("./assets/items/star/star.json").load(onAssetsLoaded);
+
+function onAssetsLoaded() {
+  console.log("WHUH ERGH");
+  console.log(Pixi.engine.Texture.fromFrame("star1.png"));
+}
+
 export default class Item {
   constructor(options) {
     this.hero = options.hero;
+
+    // console.log(Pixi.engine.Texture.fromFrame("star1.png"));
+
+    if (options.type === "star") {
+      for (var i = 0; i < 2; i++) {
+        var val = i < 10 ? "0" + i : i;
+
+        // magically works since the spritesheet was loaded with the Pixi loader
+        // frames.push(Pixi.engine.Texture.fromFrame("rollSequence00" + val + ".png"));
+      }
+    }
+
     this.texture = new Pixi.engine.Texture.fromImage(
       "./assets/items/" + options.type + ".png"
     );
